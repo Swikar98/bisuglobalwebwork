578f2913870bf6a91888bdd3f093f1c7db7e1400
Ashok <chaudharyashoka112@gmail.com>
2024-03-02 19:00:34 +0545

    minor changes

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,7 +7,7 @@ import MainCard from '@/component/MainHome/MainCard'
 import Welcome from '@/component/MainHome/Welcome'
 import DestinationMenu from "@/component/MainHome/DestinationMenu"
 import HomeBackground from "@/component/MainHome/HomeBackground"
-import Shape from "@/component/MainHome/Shape"
+import Shape from "@/component/MainHome/shape"
 import Choose from "@/component/MainHome/choose"
 export default function Home() {
   return (
