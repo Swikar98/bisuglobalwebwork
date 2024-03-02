f6a609e2647c07eb0132084838fe18db03a36274
Ashok <chaudharyashoka112@gmail.com>
2024-03-02 19:10:41 +0545

    check updated

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,14 +3,13 @@ import Text from "../../component/Text";
 import ServicesMenu from "@/component/MainHome/ServicesMenu"
 import DestinationMenu from "@/component/MainHome/DestinationMenu"
 import HomeBackground from "@/component/MainHome/HomeBackground"
-import Shape from "@/component/MainHome/Shape"
+import Shape from "@/component/MainHome/shape"
 import Choose from "@/component/MainHome/choose"
 export default function Home() {
   return (
     <>
       <HomeBackground  /> 
       <Shape />
-      
       <DestinationMenu/>
       <Choose/>
       <Text />
