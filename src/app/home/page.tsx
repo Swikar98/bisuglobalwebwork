4258360a454400b66bdbae7f5eb48a2fcba080cf
Ashok <chaudharyashoka112@gmail.com>
2024-03-02 19:03:13 +0545

    minor change

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,9 @@
 "use client";
 import Text from "../../component/Text";
-import NextJsCarousel from "../../component/NextJsCarousel";
 import ServicesMenu from "@/component/MainHome/ServicesMenu"
-import Mainaus from '@/component/MainHome/Mainaus'
-import MainCard from '@/component/MainHome/MainCard'
-import Welcome from '@/component/MainHome/Welcome'
 import DestinationMenu from "@/component/MainHome/DestinationMenu"
 import HomeBackground from "@/component/MainHome/HomeBackground"
-import Shape from "@/component/MainHome/shape"
+import Shape from "@/component/MainHome/Shape"
 import Choose from "@/component/MainHome/choose"
 export default function Home() {
   return (
